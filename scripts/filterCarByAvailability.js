8933b92c094abc77250756edeec457006c8a2c6a
Farros <86004023+farroshsy@users.noreply.github.com>
2022-09-07 19:01:33 +0700

    Update filterCarByAvailability.js

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -7,6 +7,13 @@ function filterCarByAvailability(cars) {
 
   // Tulis code-mu disini
     cars.forEach(item => item.available ? result.push(item) : '');
+  
+  //Penggunaan Looping Manual
+    // for (let i = 0; i < cars.length; i++) {
+    //   if (cars[i].available === true) {
+    //     result.push(cars[i]);
+    //   }
+    // }
 
   // Rubah code ini dengan array hasil filter berdasarkan availablity
   return result;
