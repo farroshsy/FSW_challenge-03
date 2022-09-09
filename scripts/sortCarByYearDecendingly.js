9ac9769a358b4cf518469321d272298ae7a34ac5
Farros <86004023+farroshsy@users.noreply.github.com>
2022-09-09 18:15:06 +0700

    Update sortCarByYearDecendingly.js

--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -9,20 +9,20 @@ function sortCarByYearDescendingly(cars) {
   // Tulis code-mu disini
   // Penggunaan Fungsi For Each
    result.forEach((value, i) => {
-    for (let j = 0; j < result.length-i-1; j++) {
-      if(result[j].year < result[j+1].year){
+    for(let j = 0; j < result.length - i - 1; j++) {
+      if(result[j].year < result[j + 1].year){
         let temp = result[j];
         result[j] = result[j + 1];
-        result[j+1] = temp;
+        result[j + 1] = temp;
       }
     }
   })
   
   // Penggunaan Looping Manual
-  //   for (var i = 0; i < result.length; i++) {
-  //   for (var j = 0; j < result.length - 1; j++) {
-  //     if (result[j].year < result[j + 1].year) {
-  //       var temp = result[j];
+  //   for(let i = 0; i < result.length; i++) {
+  //   for(let j = 0; j < result.length - 1; j++) {
+  //     if(result[j].year < result[j + 1].year) {
+  //       let temp = result[j];
   //       result[j] = result[j + 1];
   //       result[j + 1] = temp;
   //     }
